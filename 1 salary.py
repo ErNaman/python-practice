69a5ed264c565f09ab2d1b54c872755ab79a50f6
ErNaman <naman2334@gmail.com>
2020-02-24 23:32:06 +0530

    Add files via upload

---
+++ 1 salary.py
@@ -0,0 +1,5 @@
+salary=int(input(print("enter the basic salary:-")))
+allowance=int(input(print("enter the basic allowance:-")))
+taxes=int(input(print("enter the taxes:-")))
+total_salary=salary+allowance-taxes
+print("total salary is:- ",total_salary)
---
+++ 2 marks percentage.py
@@ -0,0 +1,9 @@
+maths=int(input(print("enter the marks of maths:- ")))
+phy=int(input(print("enter the marks of physics:- ")))
+chem=int(input(print("enter the marks of chemistry:- ")))
+eng=int(input(print("enter the marks of english:- ")))
+cs=int(input(print("enter the marks of computer science:- ")))
+total=maths+phy+chem+eng+cs
+per=(total/500)*100
+print("total marks of five subjects are:- ",total)
+print("percentage of five subjects are:- ",per)
